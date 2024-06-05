4dab68f025c0badb3138b3ec2ef7ad927830d31b
Alexandra Junges <aleexandra.junges@gmail.com>
2024-06-05 10:23:50 +0200

    project created in Alura Course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,60 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './components/container/container.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SeparatorComponent } from './components/separator/separator.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FormsModule } from '@angular/forms';
+
+interface Contact {
+  id: number
+  name: string
+  phone: string
+}
+
+import agenda from './agenda.json'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ContainerComponent,
+    HeaderComponent,
+    SeparatorComponent,
+    ContactComponent,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alphabet: string = 'abcdefghijklmnopqrstuvwxyz'
+  contacts: Contact[] = agenda;
+
+  filterByText: string = ''
+
+  private removeAccents(text: string): string {
+    return text.normalize('NFD').replace(/[\u0300-\u036F]/g, '')
+  }
+
+  filterContactsByText (): Contact[] {
+    if(!this.filterByText) {
+      return this.contacts
+    }
+    return this.contacts.filter( contact => {
+      return this.removeAccents(contact.name)
+      .toLowerCase()
+      .includes(this.removeAccents(this.filterByText)
+      .toLowerCase())
+    })
+  }
+
+  filterContactsByInitialLeter(letter: string) : Contact[] {
+    return this.filterContactsByText().filter( contact => {
+      return this.removeAccents(contact.name).toLocaleLowerCase().startsWith(this.removeAccents(letter).toLowerCase())
+    })
+  }
 }
